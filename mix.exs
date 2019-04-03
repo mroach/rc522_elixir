ff7fa05d4c387a93e53008a865f12afe020073e0
Michael Roach <michael.roach@xing.com>
2019-04-03 10:45:53 +0200

    Don't need Elixir 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RC522.MixProject do
     [
       app: :rc522,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
